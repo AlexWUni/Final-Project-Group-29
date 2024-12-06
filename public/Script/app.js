03609494fefb7e704af4a883c26e3a0806e98a71
AlexWUni <alexander.wu@ontariotechu.net>
2024-12-05 22:42:19 -0500

    Added Deletion Confirmation Message

--- public/Script/app.js
+++ public/Script/app.js
@@ -3,6 +3,17 @@
 (function(){
     function start(){
         console.log("Server Started");
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+        for(button of deleteButtons)
+            {
+                button.addEventListener('click',(event)=>{
+                    if(!confirm("Are you sure?"))
+                    {
+                        event.preventDefault();
+                        window.location.assign('/incidentslist');
+                    }
+                });
+            }
     }
     window.addEventListener("load",start);
 })();
